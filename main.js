c4abb21e2ea28be79464b57c36e8adfe717ac801
Guest <guest-zge6dl@h26632.sgb.skir.kr>
2021-01-02 15:18:29 +0900

    Incomplete storage

---
+++ main.js
@@ -0,0 +1,19 @@
+const text = document.getElementById('input_box');
+const buttons = document.querySelectorAll("#mainSection button");
+console.log(text);
+text.addEventListener('keyup',searching);
+
+function searching(){
+  buttons.forEach(element => {
+    if(text.value == null || text.value ==""){
+      element.classList.remove("hide");
+    }
+    else{
+      element.classList.add("hide");
+    }
+
+    if(element.dataset.ko.charAt(0) == text.value.charAt(0)){
+      element.classList.remove("hide");
+    }
+  });
+}
